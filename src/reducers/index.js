876c5c0f86eba64b555dc17a6324da3b6b9cc969
anhle1476 <anhle1476@gmail.com>
2021-04-28 23:28:40 +0700

    remove echart and some unuse dependencies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import navigation from './navigation';
-import alerts from './alerts';
-import register from './register';
+import { combineReducers } from "redux";
+import auth from "./auth";
+import navigation from "./navigation";
+import alerts from "./alerts";
 
 export default combineReducers({
   alerts,
   auth,
   navigation,
-  register,
 });
